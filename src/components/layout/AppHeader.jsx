b0b13ada54036bba30799957c4fb2d895911d413
Timofey <pttima@yandex.ru>
2024-02-13 15:08:36 +0300

    Header styles editing

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,17 +1,73 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button, Flex } from 'antd'
+
+const handleChange = (value) => {
+   console.log(`selected ${value}`);
+};
+
+const options = [
+   {
+      label: 'China',
+      value: 'china',
+      emoji: '🇨🇳',
+      desc: 'China (中国)',
+   },
+   {
+      label: 'USA',
+      value: 'usa',
+      emoji: '🇺🇸',
+      desc: 'USA (美国)',
+   },
+   {
+      label: 'Japan',
+      value: 'japan',
+      emoji: '🇯🇵',
+      desc: 'Japan (日本)',
+   },
+   {
+      label: 'Korea',
+      value: 'korea',
+      emoji: '🇰🇷',
+      desc: 'Korea (韩国)',
+   },
+]
+
 
 const AppHeader = () => {
    const headerStyle = {
+      width: '100%',
       textAlign: 'center',
       color: '#fff',
       height: 64,
-      paddingInline: 48,
-      lineHeight: '64px',
       backgroundColor: '#4096ff',
+      padding: '1rem',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
    }
+
    return (
       <div>
-         <Layout.Header style={headerStyle}><h1>Crypto App</h1></Layout.Header>
+         <Layout.Header style={headerStyle}>
+            <h1>Crypto App</h1>
+            <Flex gap="small" wrap="wrap">
+               <Select
+                  style={{ width: 250 }}
+                  value={'press to open'}
+                  onChange={handleChange}
+                  optionLabelProp="label"
+                  options={options}
+                  optionRender={(option) => (
+                     <Space>
+                        <span role="img" aria-label={option.data.label}>
+                           {option.data.emoji}
+                        </span>
+                        {option.data.desc}
+                     </Space>
+                  )}
+               />
+               <Button type="primary">Primary Button</Button>
+            </Flex>
+         </Layout.Header>
       </div>
    )
 }
