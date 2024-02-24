7e1c2ce5b372747f37c3fa0441a1a9680fb0674e
Timofey <pttima@yandex.ru>
2024-02-24 23:53:17 +0300

    asset summary counter crated in content component

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,4 +1,5 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+import { useCrypto } from '../../context/cryptoContext'
 
 const AppContent = () => {
    const contentStyle = {
@@ -8,9 +9,18 @@ const AppContent = () => {
       backgroundColor: '#0958d9',
       padding: '1rem'
    }
+   const { assets, crypto } = useCrypto()
    return (
       <>
-         <Layout.Content style={contentStyle}>Content</Layout.Content>
+         <Layout.Content style={contentStyle}>
+            <Typography.Title style={{ color: '#fff' }}>
+               Portfolio: $ {assets.map((asset) => {
+                  const coin = crypto.find((coin) => coin.id === asset.id)
+                  return asset.amount * coin.price
+               }).reduce((acc, value) => (acc += value), 0)
+                  .toFixed(2)}
+            </Typography.Title>
+         </Layout.Content>
       </>
    )
 }
